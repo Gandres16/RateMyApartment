777a2f589fccc883c1c7e97156b8b5d91770bdeb
Jacob Frencher <jacoblf@iastate.edu>
2024-05-05 16:38:24 -0500

    login working without sessions

--- Backend/index.js
+++ Backend/index.js
@@ -46,7 +46,6 @@ app.post("/signup", async (req, res) => {
         //TODO: Add validation for email and password
 
         // Proceed to add new user
-        const { id, title, price, description, category, image, rating } = req.body;
         const insertSql = "INSERT INTO users (email, password) VALUES (?, ?)";
         const insertResult = await db.query(insertSql, [email, password])
 
@@ -54,6 +53,60 @@ app.post("/signup", async (req, res) => {
         const msg = "POST:Success in Posting MySQL"+insertResult;
         console.log(msg);
         return res.status(200).send({success:msg});
+
+
+    } catch (err) {
+        // Handle any error
+        const msg = "POST: An ERROR occurred in Post"+err;
+        console.error(msg);
+        res.status(500).send({error:msg});
+    }
+});
+
+app.post("/login", async (req, res) => {
+    try {
+        // Validate if body contains data
+        if (!req.body || Object.keys(req.body).length === 0) {
+            const msg = "POST:Bad request: No data provided.";
+            console.log(msg);
+            return res.status(400).send({ error: msg});
+        }
+
+        // Check if the table exists
+        const [tableExists] = await db.query("SHOW TABLES LIKE 'users'");
+        if (tableExists.length === 0) {
+            const msg = "POST:Table does not exist";
+            console.log(msg);
+            return res.status(404).send({error:msg});
+        }
+
+        const email = req.body.email;
+        const password = req.body.password;
+
+        // Check if email is in database
+        const [userExists] = await db.query("SELECT * FROM users WHERE email = ?", [email]);
+        if (userExists.length < 1) {
+            // User does not exist
+            const msg = "POST:No user with that email exists";
+            console.log(msg);
+            return res.status(409).send({error:msg});
+        }
+
+        // Check if password is correct
+        const [correctPassword] = await db.query("SELECT * FROM users WHERE email = ? AND password = ?", [email, password]);
+        if (correctPassword.length < 1) {
+            // Password is incorrect
+            const msg = "POST:Password is incorrect";
+            console.log(msg);
+            return res.status(409).send({error:msg});
+        }
+
+        // success
+        const msg = "POST:User signed in successfully";
+        console.log(msg);
+        return res.status(200).send({success:msg});
+
+        // add logic for signing in user
 
 
     } catch (err) {
--- Frontend/src/appfrontend.js
+++ Frontend/src/appfrontend.js
@@ -61,16 +61,77 @@ function App() {
             <form onSubmit={handleSubmit}>
                 <h1>Signup</h1>
                 <input type="text" name="email" value={formData.email} onChange={handleChange} placeholder="Email" required /> <br />
-                <input type="text" name="password" value={formData.password} onChange={handleChange} placeholder="Password" required /> <br />
+                <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="Password" required /> <br />
                 <button type="submit">Submit</button>
             </form>
         </div>);
     }
 
+    const Login = () => {
+        // define hooks
+        const navigate = useNavigate();
+        const [formData, setFormData] = useState({
+            email: "",
+            password: ""
+        });
+
+    // Function to add input in formData HOOK using operator ...
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    // Function to fetch backend for POST - it sends data in BODY
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target.value);
+
+        fetch("http://127.0.0.1:4000/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(formData)
+        })
+
+        .then(response => {
+            if (response.status != 200){
+                return response.json()
+                .then(errData =>{
+                    throw new Error(`POST response was not ok :\n Status:${response.status}. \n Error: ${errData.error}`);
+                })
+            }
+            return response.json();})
+
+        .then(data => {
+            console.log(data);
+            alert("User logged in successfully!");
+        })
+
+        .catch(error => {
+            console.error('Error logging in:', error);
+            alert('Error logging in:'+error.message); // Display alert if there's an error
+        });
+    }
+
+    // return
+    return (<div>
+        {/* Form to input data */}
+        <form onSubmit={handleSubmit}>
+            <h1>Login</h1>
+            <input type="text" name="email" value={formData.email} onChange={handleChange} placeholder="Email" required /> <br />
+            <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="Password" required /> <br />
+            <button type="submit">Submit</button>
+        </form>
+    </div>);
+}
+
     return (
     <Router>
         <Routes>
             <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
         </Routes>
     </Router>
     );
