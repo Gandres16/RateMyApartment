bb57d780c8692d951dc158db0dea691c5f99e229
Jacob Frencher <jacoblf@iastate.edu>
2024-05-04 20:42:36 -0500

    git ignore

--- Backend/db.js
+++ Backend/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise')
 
 const db = mysql. createPool({
 host: "127.0.0.1",
-user: "mysqlusername",
-password: "****************",
-database: "secoms319"
+user: "root",
+password: "Sunflower515",
+database: "finalproject"
 })
 
 module.exports = db;
